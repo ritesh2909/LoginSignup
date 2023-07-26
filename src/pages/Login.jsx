10f8963ae928b7bf2fff22abe0a947dea29f54f8
Ritesh Arora <68393812+ritesh2909@users.noreply.github.com>
2023-07-27 00:03:07 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -15,7 +15,7 @@ function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", {
+      const res = await axios.post("https://weary-fawn-coveralls.cyclic.app/api/auth/login", {
         email: data.email,
         password: data.password,
       });
